c86b1f27a632c887bc08ccddee17f90a36586e47
harikarthik-s <harikarthiks15@gmail.com>
2024-04-09 10:55:33 +0530

    events page added

--- middleware.ts
+++ middleware.ts
@@ -16,10 +16,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",   // Match all routes except those with file extensions or under _next directory
-    "/",                               // Match root route
-    "/(api|trpc)(.*)",                  // Match routes starting with "/api" or "/trpc"
-    "/favicon.ico",                     // Match specifically the favicon.ico route
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
